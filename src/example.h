683a94037979a8f8c8a41ba3cfb515b6d9ee9f16
Robin Deits <robin.deits@gmail.com>
2015-08-19 19:37:12 -0400

    working class wrapper, but not sure if the destructor is being called

--- src/example.h
+++ src/example.h
@@ -1,5 +1,24 @@
+#include <iostream>
+
 /* A global variable */
 double Foo = 3.0;
 
 /* Compute the greatest common divisor of positive integers */
 int gcd(int x, int y);
+
+class GCDPlusX {
+public:
+  GCDPlusX(int x=0):
+    x(x) {
+      std::cout << "constructing a new object with x = " << x << std::endl;
+    }
+  int x;
+
+  int gcdPlusX(int y, int z) {
+    return gcd(y, z) + x;
+  }
+
+  ~GCDPlusX() {
+    std::cout << "destructing object at address: " << this << std::endl;
+  }
+};
