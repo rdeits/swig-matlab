de5a837b15ad6b2d3ca00c1bcefd7419cf0b71ad
Wolfgang Merkt <w.merkt@gmail.com>
2016-01-19 15:17:19 +0000

    Add bison (required to compile drake)

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -4,7 +4,7 @@ case $1 in
   ("homebrew")
     brew install cmake pcre ;;
   ("ubuntu")
-    apt-get install cmake build-essential yodl ;;
+    apt-get install cmake build-essential yodl bison ;;
   (*)
     echo "Usage: ./install_prereqs.sh package_manager"
     echo "where package_manager is one of the following: "
